7c88af2e14296bec3d0d3b889f96a0eb416ef742
Joe Heck <heckj@mac.com>
2021-11-29 15:32:31 -0800

    Initial Commit

---
+++ IsosurfaceExtraction/IsosurfaceExtractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsosurfaceExtractionApp.swift
+//  IsosurfaceExtraction
+//
+//  Created by Joseph Heck on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct IsosurfaceExtractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
