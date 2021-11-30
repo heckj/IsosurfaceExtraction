ccc684e760859314baba64402b543b04e41db755
Joe Heck <heckj@mac.com>
2021-11-30 15:59:05 -0800

    expose a basic Euclid generated SceneKit view as a stepping stone

--- IsosurfaceExtraction/ContentView.swift
+++ IsosurfaceExtraction/ContentView.swift
@@ -6,16 +6,62 @@
 //
 
 import SwiftUI
+import SceneKit
+import Euclid
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var scene: SCNScene
+            
+    var cameraNode: SCNNode? {
+        scene.rootNode.childNode(withName: "camera", recursively: false)
     }
+    
+    var body: some View {
+        VStack {
+            Text("My SceneKit View")
+            SceneView(
+                    scene: scene,
+                    pointOfView: cameraNode,
+                    options: [.allowsCameraControl, .autoenablesDefaultLighting]
+            )
+            // These don't appear to be exposed into SwiftUI view land...
+            //    scnView.showsStatistics = true
+            //    scnView.backgroundColor = .white
+        }
+    }
+}
+
+func provideEuclidScene() -> SCNScene {
+    // create a new scene
+    let scene = SCNScene()
+
+    // create and add a camera to the scene
+    let cameraNode = SCNNode()
+    cameraNode.name = "camera"
+    cameraNode.camera = SCNCamera()
+    scene.rootNode.addChildNode(cameraNode)
+
+    // place the camera
+    cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
+
+    // create some geometry using Euclid
+    let start = CFAbsoluteTimeGetCurrent()
+    let cube = Mesh.cube(size: 0.8, material: UIColor.red)
+    let sphere = Mesh.sphere(slices: 120, material: UIColor.blue)
+    let mesh = cube.subtract(sphere)
+    print("Time:", CFAbsoluteTimeGetCurrent() - start)
+    print("Polys:", mesh.polygons.count)
+
+    // create SCNNode
+    let geometry = SCNGeometry(mesh)
+    let node = SCNNode(geometry: geometry)
+    scene.rootNode.addChildNode(node)
+
+    return scene
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(scene: provideEuclidScene())
     }
 }
--- IsosurfaceExtraction/IsosurfaceExtractionApp.swift
+++ IsosurfaceExtraction/IsosurfaceExtractionApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct IsosurfaceExtractionApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(scene: provideEuclidScene())
         }
     }
 }
